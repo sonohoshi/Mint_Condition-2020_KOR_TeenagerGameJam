97c9373b8e983cb57c96321f0e2939db4fe54289
sonohoshi <yd111544@gmail.com>
2020-09-18 23:52:19 +0900

    lage files

---
+++ MintCondition/MintCondition/Assets/Scripts/KangDH/ShootEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootEffect : MonoBehaviour
+{
+    private Animator shootEffectAnim;
+    private bool shootSign;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootEffectAnim = GetComponent<Animator>();
+        Shoot();
+    }
+    public void Shoot()
+    {
+        Debug.Log($"shootEffectAnim : {shootEffectAnim}");
+        if (shootEffectAnim == null)
+        {
+            shootEffectAnim = GetComponent<Animator>();
+        }
+        gameObject.SetActive(true);
+        shootEffectAnim.Play("ShootEffect");
+        Invoke("EffectEnd",0.5f);
+    }
+
+    private void EffectEnd()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ MintCondition/MintCondition/Assets/Scripts/KimSM/CSVParser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVParser : MonoBehaviour
+{
+    private static readonly string mapCSVPath = "CSVs/map_";
+
+    public static string[] GetMapFile(int stage)
+    {
+        string path = mapCSVPath + stage.ToString();
+        var ta = Resources.Load(path) as TextAsset;
+        return ta.text.Split('\n');
+    }
+}
