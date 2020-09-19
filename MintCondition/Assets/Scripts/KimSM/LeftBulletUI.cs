88b1ba4ee272a6ddc6dd3d0b6692d837ce39ecc6
sonohoshi <yd111544@gmail.com>
2020-09-19 15:28:45 +0900

    Add UI showing remaining bullet

--- MintCondition/Assets/Scripts/KimSM/LeftBulletUI.cs
+++ MintCondition/Assets/Scripts/KimSM/LeftBulletUI.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeftBulletUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Text text;
+
     void Start()
     {
-        
+        text = GetComponent<Text>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void LateUpdate()
     {
-        
+        text.text = $"남은 총알 : {GameManager.Instance.MaxBullets[PrivateSceneManager.Manager.nowStage - 1]}";
     }
 }
